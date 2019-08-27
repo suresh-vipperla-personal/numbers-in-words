e5ceef40424bd8719309401742fa5e6f24fcd36f
suresh-vipperla-personal <53182897+suresh-vipperla-personal@users.noreply.github.com>
2019-08-28 01:00:20 +0530

    Update SpeakNumbers.scala

--- src/main/scala/com/starter/codingtest/SpeakNumbers.scala
+++ src/main/scala/com/starter/codingtest/SpeakNumbers.scala
@@ -26,8 +26,8 @@ object SpeakNumbers {
 
   ////This is an inorder traversal , We have to process the left i.e the quotient , then on the right   the remainder
   //// For example 563,732  will be decomposed into process(563) + " thousands " + process (732)
-  //// For numbers till about a trillion for this exercise the tree will not be 10 levels so this is not inefficient
-  //// CIf we can make this tailrec it would be even better...
+  //// For numbers till about a trillion for this exercise, the tree will not be > 10 levels, so this is not inefficient
+  //// If we can make this tailrec it would be even better...
   def numbersToWords(num: Long, printZero: Boolean = true): String = {
     ////Setting Upper and Lower bounds as 1 trillion (1000000000000)
     if (num>999999999999L || num< -999999999999L) return s"Please enter a number less than 1000000000000(one trillion)"
