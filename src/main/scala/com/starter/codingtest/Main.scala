230ade026a5c6795e85fbcb227cb8a31c2dc044d
Suresh Vipperla <sanathan@gmail.com>
2019-08-27 15:48:23 +0530

    Fixed a build failure and gradle build file corrections

--- src/main/scala/com/starter/codingtest/Main.scala
+++ src/main/scala/com/starter/codingtest/Main.scala
@@ -5,15 +5,16 @@ object Main {
   def main(args: Array[String]) {
     while (true) {
       Console.println("Please input a number less than trillion without decimals...")
-      val exceptionMessage="Invalid Input : Please input a number less than a Trillion without decimals..."
+      val exceptionMessage="Invalid Input! "
       try {
         val input = scala.io.StdIn.readLong()
         if (input>999999999999L || input< -999999999999L) Console.println (exceptionMessage)
-        else Console.println(s"The Word representation  of $input is \" ${SpeakNumbers.numbersToWords(input)} \"")
+        else Console.println(s"The Word representation  of $input is :  '${SpeakNumbers.numbersToWords(input)}'" )
       }
       catch
         {
           case nfex: NumberFormatException => Console.println(exceptionMessage)
+          case gex:Exception => Console.println("Unknown Exception, Please try again later") ; Console.flush()
         }
     }
   }
